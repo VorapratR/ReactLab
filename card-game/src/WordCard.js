daf38f0a22a51fcb21bec4f39da8d604f703c6d4
VorapratR <vorapratrattana@gmail.com>
2018-09-17 11:55:02 +0700

    Handler as Props : เป็นการส่งค่ายอดกลับในหายอด จาก CharacterCard

--- card-game/src/WordCard.js
+++ card-game/src/WordCard.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import CharacterCard from './CharacterCard';
 export default class WordCard extends Component{
+    activate =  c => { console.log(`${c} has been activated.`) } 
+      
+    activateHandler = c => {
+        console.log(`${c} has been activate`)
+    }
+
     render(){
         return(
             <div>
-            { Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i}/>) }
+            {Array.from(this.props.value).map((c,i) => <CharacterCard value={c} key= {i} activationHandler={this.activateHandler}/>)}
+            ae
             </div>
         );
     }
