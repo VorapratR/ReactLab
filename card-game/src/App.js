e3ff9063973aba1ecd566cee578d0765b6643950
VorapratR <vorapratrattana@gmail.com>
2018-09-17 10:53:20 +0700

    More : ทดลองสร้าง Tree หลายชั้นมากขึ้น

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CharacterCard from './CharacterCard';
+import WordCard from './WordCard';
 
 const word = "Hello"
 class App extends Component {
@@ -9,11 +10,7 @@ class App extends Component {
   render() {
     return (
       <div >
-       
-       {
-         Array.from(word).map((c,i)=> <CharacterCard value={c} key={i}/>)
-       }
-       
+       <WordCard value="hello"/>
       </div>
     );
   }
