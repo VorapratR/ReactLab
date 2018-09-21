081cf9746922b1911747fd7da3b10e3515924cbc
VorapratR <vorapratrattana@gmail.com>
2018-09-21 17:01:14 +0700

    ทดลอง  ใส่ video

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -7,14 +7,19 @@ class App extends Component {
   
   render() {
     return (
-      <div className="App">
+      /*<div className="App">
       {
         <p className="Login">NAME :    
         <input type="text" onBlur={this.onBlur} /></p>
         
-      }<img src ='./Star-Wars-Millenium-Falcon-Render.jpg'/> 
+      }<img src ='http://www.jwam.com.au/media/images/easy-photogallery/Star-Wars-Millenium-Falcon-Render.jpg'/> 
       <WordCard value = {word}/> 
-      </div>
+      </div>*/
+      <video id="background-video" loop autoPlay>
+    <source src="http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4" type="video/mp4" />
+    <source src="http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4" type="video/ogg" />
+    Your browser does not support the video tag.
+</video>
     );
   }
 }
