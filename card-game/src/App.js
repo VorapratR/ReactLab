6277615c9f9d7333344f9ef444a2a99b80af6c7d
VorapratR <vorapratrattana@gmail.com>
2018-09-21 17:38:31 +0700

    จัด login

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -8,13 +8,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      {
+        <WordCard value = {word}/>
         <p className="Login">NAME :    
         <input type="text" onBlur={this.onBlur} />
         </p>
-        
-      }<img src ='http://www.jwam.com.au/media/images/easy-photogallery/Star-Wars-Millenium-Falcon-Render.jpg'/> 
-      <WordCard value = {word}/> 
+        <img src ='http://www.jwam.com.au/media/images/easy-photogallery/Star-Wars-Millenium-Falcon-Render.jpg'/>  
       </div>
     );
   }
